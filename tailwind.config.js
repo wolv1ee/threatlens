129e0c1b4841711231b08345c002f83980803e4f
Saad Mahmud <mdsaad111.s3@gmail.com>
2026-05-24 15:30:00 -0400

    add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,3 +11,32 @@ module.exports = {
   },
   plugins: [],
 }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  safelist: [
+    'btn-hover',
+    'card-hover',
+    'tab-hover',
+    'nav-hover',
+    'link-hover',
+    'animate-fade-in-up',
+    'animate-slide-in',
+    'animate-pulse-glow',
+    'stat-animate',
+    'delay-1',
+    'delay-2',
+    'delay-3',
+    'delay-4',
+    'delay-5',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['"JetBrains Mono"', 'monospace'],
+        sans: ['"DM Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
